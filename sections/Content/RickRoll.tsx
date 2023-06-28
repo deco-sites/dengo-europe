3b994438dc1058ea076f23c36301fc94871bac03
Lucas Yamamoto <17356081+lucasayb@users.noreply.github.com>
2023-06-28 17:08:33 -0400

    adds rick roll

---
+++ sections/Content/RickRoll.tsx
@@ -0,0 +1,14 @@
+export default function RickRoll() {
+  return (
+    <iframe
+      width="100%"
+      height="315"
+      src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+    >
+    </iframe>
+  );
+}
